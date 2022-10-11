2eccd55789c9b68ef99cf293f23052862098977f
gustavojb1 <gustavojunqueirab@gmail.com>
2022-10-11 15:40:11 -0300

    finished

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,20 +1,20 @@
-// const addElement = () => {
-//     const lista = document.getElementById("lista")
-// //cria elemento
-//     const item0 = document.createElement("li")
-//     item0.innerHTML = "item 0"
+const addElement = () => {
+    const lista = document.getElementById("lista")
+//cria elemento
+    const item0 = document.createElement("li")
+    item0.innerHTML = "item 0"
 
-// //insere elemento no inicio
-//     lista.insertAdjacentElement('afterbegin', item0)
+//insere elemento no inicio
+    lista.insertAdjacentElement('afterbegin', item0)
 
-//     const item5 = document.createElement("li")
-//     //obrigatoriamente no final
-//     lista.appendChild(item5)
-//     item5.innerHTML = "item 5"
-// }
+    const item5 = document.createElement("li")
+    //obrigatoriamente no final
+    lista.appendChild(item5)
+    item5.innerHTML = "item 5"
+}
 
 
-// addElement()
+addElement()
 
 const addToList = () => {
     const container = document.getElementById("container")
